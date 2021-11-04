2c474e9b58c9fa4b4c595e236f3fd94e0fac2af3
97jorge <93451402+97jorge@users.noreply.github.com>
2021-11-04 11:31:16 -0600

    Add files via upload

---
+++ proyectoJorgeGonzalezFerreteria/resources/views/ferreteria.blade.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+   
+    <title>Ferreteria Jorge</title>
+</head>
+<style>
+ div{
+    
+  font-size: 50px;
+    font-style: italic;
+    background-color:aquamarine;
+ }
+
+</style>
+
+<body>
+
+<div>
+    <h1>ferreteria jorge</h1>
+</div>
+    <h2>Aqui encuentras todo lo que necesitas</h2>
+
+</body>
+</html>
